4bf97962015bc9c91914bdebc4e9069f78b7b610
Kyle Ellrott <kellrott@gmail.com>
2019-03-04 17:01:47 -0800

    Working on markdown build

---
+++ gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -e build ]; then
+  mkdir build
+fi
+
+for i in *.ipynb; do
+  b=`basename $i | sed s'/.ipynb//'`
+  if [ ! -e build ]; then
+    mkdir build/$b
+  fi
+  jupyter nbconvert --to markdown ${i} --output-dir build/$b --output _index.md
+done
